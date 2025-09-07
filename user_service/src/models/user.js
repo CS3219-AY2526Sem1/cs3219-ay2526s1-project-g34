803aa16ea2707f57d18b36a285d80bc8c7ed3ec6
Toh Heng Xing <102740235+tohhengxing@users.noreply.github.com>
2025-09-08 00:47:06 +0800

    feat: Add user model and migration

---
+++ user_service/src/models/user.js
@@ -0,0 +1,58 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    async comparePassword(password) {
+      return bcrypt.compare(password, this.password);
+    }
+
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      defaultValue: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 50]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6, 100]
+      }
+    }
+  }, 
+  {
+    sequelize,
+    modelName: 'User',
+    hooks: {
+      beforeCreate: async (user) => {
+        user.password = await bcrypt.hash(user.password, 12);
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          user.password = await bcrypt.hash(user.password, 12);
+        }
+      }
+    }
+  });
+  return User;
+};
