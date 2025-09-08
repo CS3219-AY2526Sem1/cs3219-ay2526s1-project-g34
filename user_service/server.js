6e35b437607d3ef5748747b69a7caf5ca8886332
Toh Heng Xing <102740235+tohhengxing@users.noreply.github.com>
2025-09-08 17:05:29 +0800

    chore: Update port for user service

--- user_service/server.js
+++ user_service/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: '.env', debug: true })
 const app = require('./src/app.js');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`User service running on port ${PORT}`);
