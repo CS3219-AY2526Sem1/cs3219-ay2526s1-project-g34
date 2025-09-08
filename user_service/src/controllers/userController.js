3e221abe6c406b02bef277cdeeddaec97ab4934f
Toh Heng Xing <102740235+tohhengxing@users.noreply.github.com>
2025-09-08 12:55:42 +0800

    chore: Remove login from user controller

--- user_service/src/controllers/userController.js
+++ user_service/src/controllers/userController.js
@@ -20,19 +20,6 @@ const userController = {
           res.status(500).json({ error: error.message });
         }
     },
-    login: async (req, res) => {
-        try {
-            const user = await User.findOne({ where: { username: req.body.username }});
-            if (!user) {
-                return res.status(404).json({ error: 'User not found' });
-            }
-
-            await user.comparePassword(req.body.password);
-            res.json(user);
-        } catch (error) {
-            res.status(401).json({ error: "Incorrect username or password"})
-        }
-    },
 };
 
 module.exports = { userController }
