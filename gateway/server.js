833999f3ccb5b3014bb8383915305dfee39ab870
Toh Heng Xing <102740235+tohhengxing@users.noreply.github.com>
2025-09-09 13:34:44 +0800

    fix: Change order of routes
    Apparently, order matters in implementing routing in express

--- gateway/server.js
+++ gateway/server.js
@@ -6,20 +6,17 @@ var app = express()
 
 app.use(morgan('dev'))
 // Debug middleware
-app.use((req, res, next) => {
-    console.log('hello')
-    console.log(`${req.method} ${req.path} - Origin: ${req.headers.origin}`);
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('hello')
+//     console.log(`${req.method} ${req.path} - Origin: ${req.headers.origin}`);
+//     next();
+// });
 app.use(cors({
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 }))
-
-app.use(express.json());
-
 
 app.use('/api/users', createProxyMiddleware({
     target: 'http://localhost:3002',
@@ -38,9 +35,12 @@ app.use('/api/auth', createProxyMiddleware({
             console.log(req.originalUrl);
             console.log(req.path);
             console.log(proxyReq.path);
+            console.log(req.body)
         }
     }
 }))
+
+app.use(express.json());
 
 app.listen(3001, function () {
   console.log('app proxy')
