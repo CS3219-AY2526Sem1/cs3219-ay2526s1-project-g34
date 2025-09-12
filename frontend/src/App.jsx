1d6c5a6e50610b9832efd91d917a5ae92a67a77d
Toh Heng Xing <102740235+tohhengxing@users.noreply.github.com>
2025-09-12 20:03:53 +0800

    chore: Fix formatting
    chore: Fix formatting
    
    chore: Fix formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ProtectedRoute } from './utils/ProtectedRoute';
-import { useState, useEffect   } from 'react';
+import { useState } from 'react';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
 
 function App() {
   const [user, setUser] = useState(null);
-  
+
   return (
     <BrowserRouter>
       <div>
         <Routes>
-        <Route path="/" element={<LoginPage setUser={setUser} />} />
+          <Route path="/" element={<LoginPage setUser={setUser} />} />
           <Route path="/home"
             element={
               <ProtectedRoute user={user}>
                 <HomePage />
               </ProtectedRoute>
             } />
-
         </Routes>
       </div>
     </BrowserRouter>
